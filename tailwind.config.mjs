385184ae3cd2eaa82bb71522349e0f36f2bec3fa
LasriOumaima <oumaimalasrihp2023@gmail.com>
2025-02-17 21:25:28 +0100

    Updated project with new images and changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,11 +9,9 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+      fontFamily:{ burtons:"burtons",},
+    },  
   },
   plugins: [],
+
 };
